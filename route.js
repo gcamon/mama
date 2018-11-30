7a76ff920ecb244416b22dfe0e375c28359a2d94
gcamon <ede.obinna27@gmail.com>
2018-11-30 14:01:38 +0100

    settings

--- route.js
+++ route.js
@@ -10,7 +10,7 @@ module.exports = function (model) {
       if(err) throw err;
         res.render('index',{single: {},others: data2});
     });
-    model.article.remove({},function(err,info){})
+    //model.article.remove({},function(err,info){})
     //res.render('index');
   });
 
